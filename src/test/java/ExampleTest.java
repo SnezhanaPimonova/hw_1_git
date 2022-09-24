f8ade1ef978323fbcd46520ead8ba9029e632cc0
unknown <bestqap@gmail.com>
2022-09-24 21:23:41 +0300

    fix logic

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -6,6 +6,10 @@ public class ExampleTest {
 
     @Test
     void exampleTest(){
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(2>=1);
+    }
+    @Test
+    void exampleTest(){
+        Assertions.assertTrue(3>=2);
     }
 }
