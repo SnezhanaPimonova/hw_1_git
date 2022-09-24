03ee8830abfc6fdcf628f954b534f200b87a9234
SnezhanaPimonova <bestqap@gmail.com>
2022-09-25 00:06:09 +0300

    conflict

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -5,10 +5,10 @@ public class ExampleTest {
 
     @Test
     void exampleTest0(){
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(2>10);
     }
     @Test
     void exampleTest1(){
-        Assertions.assertTrue(3>2);
+        Assertions.assertTrue(3>10);
     }
 }
